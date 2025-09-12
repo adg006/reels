ec495a531c9b9fd887163537e1a2444b1c82b514
Arindam Dasgupta <arindamdasgupta6@gmail.com>
2025-09-12 13:07:10 +0530

    added partner controller

---
+++ backend/src/controllers/partner.controller.js
@@ -0,0 +1,90 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import partnerModel from "../models/partner.model.js";
+
+// PARTNER REGSTRATION
+const registerPartner = async (req, res) => {
+  try {
+    const { firstName, lastName, email, password } = req.body;
+
+    const existingPartner = await partnerModel.findOne({ email });
+
+    if (existingPartner) return res.status(400).json({ message: "Partner already exists" });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newPartner = new partnerModel({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+    });
+
+    const token = jwt.sign(
+      {
+        id: newUser._id,
+      },
+      process.env.JWT_SECRET
+    );
+
+    res.cookie("token", token);
+
+    await newUser.save();
+
+    res.status(201).json({
+      message: "Partner registered successfully",
+      newPartner: {
+        id: newPartner._id,
+        email: newPartner.email,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+
+// LOGIN PARTNER
+const loginPartner = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const existingPartner = await partnerModel.findOne({ email });
+
+    if (!existingPartner) return res.status(400).json({ message: "Partner does not exist" });
+
+    const isPasswordCorrect = await bcrypt.compare(password, existingPartner.password);
+
+    if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials" });
+
+    const token = jwt.sign(
+      {
+        id: existingUser._id,
+      },
+      process.env.JWT_SECRET
+    );
+
+    res.cookie("token", token);
+
+    res.status(200).json({
+      message: "Partner logged in successfully",
+      existingPartner: {
+        id: existingPartner._id,
+        email: existingPartner.email,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+
+// LOGOUT PARTNER
+const logoutPartner = (req, res) => {
+  res.clearCookie("token");
+  res.status(200).json({ message: "Partner logged out successfully" });
+};
+
+export { registerPartner, loginPartner, logoutPartner };
