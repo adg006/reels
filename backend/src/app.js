8cc1b921c8bc4b6b0c9ce86896b5195211a13733
Arindam Dasgupta <arindamdasgupta6@gmail.com>
2025-09-16 00:08:48 +0530

    added food routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
+import foodRoutes from "./routes/food.route.js";
 
 const app = express();
 
@@ -12,5 +13,6 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/auth", authRoutes);
+app.use("/api/food", foodRoutes);
 
 export default app;
