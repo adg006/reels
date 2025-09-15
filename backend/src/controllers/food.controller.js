4a835b87e1223625fee07b6642f1ec9b52ba695a
Arindam Dasgupta <arindamdasgupta6@gmail.com>
2025-09-16 00:17:22 +0530

    added food controller

---
+++ backend/src/controllers/food.controller.js
@@ -0,0 +1,7 @@
+import foodModel from "../models/food.model";
+
+const createFood = async (req, res) => {
+  const partnerId = req.partnerId;
+};
+
+export { createFood };
--- backend/src/routes/food.route.js
+++ backend/src/routes/food.route.js
@@ -1,7 +1,9 @@
 import express from "express";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
+import { createFood } from "../controllers/food.controller.js";
 
 const router = express.Router();
 
-router.post("/", (req, res) => {});
+router.post("/", authMiddleware, createFood);
 
 export default router;
